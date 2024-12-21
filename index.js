229f8353b4ee02a417ad0a43ed38a92c461c361f
MohitR1999 <mohitr1999@gmail.com>
2024-12-21 18:21:16 +0530

    Added initial code for basic functionality and stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello world!!!!");
+})
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`This Server now running on port : ${PORT}`);
+})
