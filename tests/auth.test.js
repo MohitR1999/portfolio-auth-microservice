617285f249ce9a6d365270f26e86edbb6ff75f81
MohitR1999 <mohitr1999@gmail.com>
2024-12-26 20:34:14 +0530

    Added all the remaining test cases for auth service

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -150,6 +150,77 @@ describe('User login', () => {
         expect(res.body.error).toBe(Errors.USER_NOT_FOUND);
     });
 
+    it('Should throw error when username is missing', async () => {
+        const res = await request(app).post('/api/auth/login').send(
+            {
+                password : `${testUser.password}`
+            }
+        );
+
+        expect(res.statusCode).toBe(Errors.UNSUCCESSFUL_ERROR_STATUS);
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe(Errors.EMPTY_USERNAME);
+    });
+
+    it('Should throw error when password is missing', async () => {
+        const res = await request(app).post('/api/auth/login').send(
+            {
+                username : `${testUser.username}`
+            }
+        );
+
+        expect(res.statusCode).toBe(Errors.UNSUCCESSFUL_ERROR_STATUS);
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe(Errors.EMPTY_PASSWORD);
+    });
+
+
+})
+
+describe('User logout', () => {
+    const testUser = {
+        "username": "john",
+        "first_name": "John", 
+        "last_name": "Doe", 
+        "email": "john@example.com", 
+        "password": "123456"
+    }
+
+    beforeAll(async () => {
+        await connectDB(globalThis.__MONGO_URI__);
+    })
+
+    afterAll(async () => {
+        await mongoose.connection.dropDatabase();
+        await mongoose.connection.close();
+    })
+    
+    it('Should register a user with given data', async () => {
+        const res = await request(app).post('/api/auth/register').send(testUser);
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toBe(Success.SUCCESSFUL_USER_REGISTRATION);
+        expect(res.body).toHaveProperty("userId");
+    });
+
+    it('Should login the user when correct credentials are given', async () => {
+        const res = await request(app).post('/api/auth/login').send(
+            {
+                username : testUser.username,
+                password : testUser.password
+            }
+        );
+
+        expect(res.statusCode).toBe(Success.SUCCESSFUL_STATUS);
+        expect(res.body).toHaveProperty('token');
+    });
+
+    it('Should logout the user when logout API is hit', async () => {
+        const res = await request(app).post('/api/auth/logout').send();
+        expect(res.statusCode).toBe(Success.SUCCESSFUL_STATUS);
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toBe(Success.SUCCESSFUL_LOGOUT);
+    });
 
 })
 
