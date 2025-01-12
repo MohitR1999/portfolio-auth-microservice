a1d46426c46435d0057878589ca72a835f669b98
MohitR1999 <mohitr1999@gmail.com>
2025-01-12 13:28:37 +0530

    Added endpoint for deleting the user details

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, logout, verify } =  require('../controllers/auth.controller');
+const { register, login, logout, verify, deleteUser } =  require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 router.get('/verify', verify);
+router.delete('/user', deleteUser);
 
 module.exports = router;
